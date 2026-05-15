4596413570fa0324d95552530c34288d55b1279c
AryanNarhe <131742070+aryan040@users.noreply.github.com>
2026-05-15 10:02:30 +0530

    Add files via upload

---
+++ Simplechatbot.py
@@ -0,0 +1,17 @@
+print("Simple Chatbot")
+
+while True:
+    user=input("You: ")
+    
+    if user.lower()=="hello":
+        print("Bot: Hi")
+    
+    elif user.lower()=="how are you":
+        print("Bot: I am fine")
+        
+    elif user.lower()=="bye":
+        print("Bot: Goodbye")
+        break
+    
+    else:
+        print("Bot: Sorry, I don't understand")
